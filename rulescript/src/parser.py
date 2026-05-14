5f4894e11602078669b197b902bf2b10cb8769ff
Aman <manupolice12@gmail.com>
2026-05-14 13:38:55 +0530

    first commit

--- rulescript/src/parser.py
+++ rulescript/src/parser.py
@@ -1,81 +1,101 @@
+import ast
+
 class Parser:
     def __init__(self, tokens):
         self.tokens = tokens
-        self.position = 0
+        self.pos = 0
 
     def current_token(self):
-        if self.position < len(self.tokens):
-            return self.tokens[self.position]
-        return None
+        return self.tokens[self.pos] if self.pos < len(self.tokens) else None
 
     def advance(self):
-        self.position += 1
+        self.pos += 1
+
+    def ast_to_dict(self, node):
+        if isinstance(node, (ast.Module, ast.Expression)):
+            return self.ast_to_dict(node.body)
+        elif isinstance(node, ast.Expr):
+            return self.ast_to_dict(node.value)
+        elif isinstance(node, ast.BinOp):
+            return {
+                "type": "binary_operation",
+                "left": self.ast_to_dict(node.left),
+                "op": type(node.op).__name__,
+                "right": self.ast_to_dict(node.right)
+            }
+        elif isinstance(node, ast.Compare):
+            return {
+                "type": "comparison",
+                "left": self.ast_to_dict(node.left),
+                "operator": type(node.ops[0]).__name__,
+                "right": self.ast_to_dict(node.comparators[0])
+            }
+        elif isinstance(node, ast.Attribute):
+            return {
+                "type": "attribute_access",
+                "object": self.ast_to_dict(node.value),
+                "property": node.attr
+            }
+        elif isinstance(node, ast.Name):
+            return {"type": "variable", "id": node.id}
+        elif isinstance(node, ast.Constant):
+            return {"type": "literal", "value": node.value}
+        elif isinstance(node, ast.Call):
+            return {
+                "type": "method_call",
+                "func": self.ast_to_dict(node.func),
+                "args": [self.ast_to_dict(arg) for arg in node.args]
+            }
+        elif isinstance(node, ast.Assign):
+            return {
+                "type": "assignment",
+                "target": self.ast_to_dict(node.targets[0]),
+                "value": self.ast_to_dict(node.value)
+            }
+        return str(node)
+
+    def parse_logic_block(self, stop_tokens):
+        content = []
+        while self.current_token() and self.current_token().type not in stop_tokens:
+            content.append(self.current_token().value)
+            self.advance()
+        
+        raw_string = " ".join(content)
+        try:
+            # Use mode='exec' for 'then' blocks as they contain assignments
+            # Use mode='eval' for 'when' blocks as they are expressions
+            mode = 'exec' if '=' in raw_string else 'eval'
+            tree = ast.parse(raw_string.strip(), mode=mode)
+            return self.ast_to_dict(tree)
+        except Exception:
+            return {"type": "raw", "value": raw_string}
+
+    def parse_rule(self):
+        rule_data = {"name": "", "condition": {}, "action": {}}
+        
+        if self.current_token() and self.current_token().type == "RULE":
+            self.advance()
+            if self.current_token() and self.current_token().type == "IDENTIFIER":
+                rule_data["name"] = self.current_token().value
+                self.advance()
+
+            while self.current_token() and self.current_token().type != "RULE":
+                if self.current_token().type == "WHEN":
+                    self.advance()
+                    rule_data["condition"] = self.parse_logic_block(["THEN", "RULE"])
+                elif self.current_token().type == "THEN":
+                    self.advance()
+                    rule_data["action"] = self.parse_logic_block(["WHEN", "RULE"])
+                else:
+                    self.advance()
+                    
+        return rule_data
 
     def parse(self):
         rules = []
-
-        while self.current_token() is not None:
-            token = self.current_token()
-
-            if token.type == "RULE":
+        while self.current_token():
+            if self.current_token().type == "RULE":
                 rules.append(self.parse_rule())
             else:
                 self.advance()
-
         return rules
-
-    def parse_rule(self):
-        self.advance()  
-        name_token = self.current_token()
-        rule_name = name_token.value
-        self.advance()
-        if self.current_token() and self.current_token().type == "COLON":
-            self.advance()
-
-        condition = None
-        action = None
-
-        while self.current_token() is not None:
-            token = self.current_token()
-
-            if token.type == "WHEN":
-                self.advance()
-
-                condition_parts = []
-
-                while (
-                    self.current_token() is not None
-                    and self.current_token().type != "THEN"
-                ):
-                    condition_parts.append(str(self.current_token().value))
-                    self.advance()
-
-                condition = " ".join(condition_parts)
-
-            elif token.type == "THEN":
-                self.advance()
-
-                if self.current_token() and self.current_token().type == "COLON":
-                    self.advance()
-
-                action_parts = []
-
-                while (
-                    self.current_token() is not None
-                    and self.current_token().type != "RULE"
-                ):
-                    action_parts.append(str(self.current_token().value))
-                    self.advance()
-
-                action = " ".join(action_parts)
-                break
-
-            else:
-                self.advance()
-
-        return {
-            "type": "rule",
-            "name": rule_name,
-            "condition": condition,
-            "action": action
-        }
